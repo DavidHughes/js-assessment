cb8d133cc033b83da716623c480ec934df2d2c97
David Hughes <david.hughes125@gmail.com>
2013-09-19 23:50:34 +0100

    Fizzing and-a buzzin'

--- app/flowControl.js
+++ app/flowControl.js
@@ -3,14 +3,17 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     fizzBuzz : function(num) {
-      // write a function that receives a number as its argument;
-      // if the number is divisible by 3, the function should return 'fizz';
-      // if the number is divisible by 5, the function should return 'buzz';
-      // if the number is divisible by 3 and 5, the function should return
-      // 'fizzbuzz';
-      //
-      // otherwise the function should return the number, or false if no number
-      // was provided or the value provided is not a number
+      if (typeof num !== 'number') {
+        return false;
+      }
+
+      var fizzbuzz = [(num % 3 === 0), (num % 5 === 0)];
+      
+      if (fizzbuzz[0] || fizzbuzz[1]) {
+        return (fizzbuzz[0] ? 'fizz' : '') + (fizzbuzz[1] ? 'buzz' : '');
+      }
+
+      return num;
     }
   };
 });

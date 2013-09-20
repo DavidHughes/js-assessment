d3c988fdf4ba996db5666a9d22ae82bae12859db
David Hughes <david.hughes125@gmail.com>
2013-09-20 13:55:19 +0100

    Finished all to closures. Must be a better way to do that one

--- app/functions.js
+++ app/functions.js
@@ -3,19 +3,33 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     argsAsArray : function(fn, arr) {
-
+        return fn.apply(null, arr);
     },
 
     speak : function(fn, obj) {
-
+        return fn.apply(obj);
     },
 
     functionFunction : function(str) {
-
+        return function(str2) {
+            return str + ', ' + str2;
+        };
     },
 
     makeClosures : function(arr, fn) {
+        var squareMaker = function(x) {
+            return function() {
+                return fn(x);
+            };
+        };
 
+        var closures = [];
+
+        for (var i = 0; i < arr.length ; i++) {
+            closures.push(squareMaker(arr[i]));
+        }
+
+        return closures;
     },
 
     partial : function(fn, str1, str2) {

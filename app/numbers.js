5622fa116477c87b4758c191aad369027eddcdad
David Hughes <david.hughes@cancer.org.uk>
2013-10-31 17:03:19 +0000

    I can count to 1

--- app/numbers.js
+++ app/numbers.js
@@ -3,15 +3,41 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function() {
   return {
     valueAtBit: function(num, bit) {
-
+        return 1 & (num >> (bit - 1));
     },
 
     base10: function(str) {
+        var bitwise = str.split('').reverse().join('');
+        var base10 = 0;
 
+        for (var i = 0; i < bitwise.length; i++) {
+            if (bitwise.charAt(i) === '1') {
+                base10 = base10 + Math.pow(2, i);
+            }
+        }
+
+        return base10;
     },
 
     convertToBinary: function(num) {
+        var maxCol = 1,
+            remaining = num,
+            bitwise = '';
 
+        for (var i = 0; maxCol < num; i++) {
+            maxCol = Math.pow(2, i);
+        }
+
+        for (; maxCol >= 1; maxCol = maxCol / 2) {
+            if (maxCol <= remaining) {
+                remaining = remaining - maxCol;
+                bitwise = bitwise + '1';
+            } else {
+                bitwise = bitwise + '0';
+            }
+        }
+
+        return bitwise;
     },
 
     multiply: function(a, b) {
@@ -19,4 +45,3 @@ define(function() {
     }
   };
 });
-

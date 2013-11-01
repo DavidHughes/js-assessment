b5237c674b741e4ff2ee7edf6066d3a13782f840
David Hughes <david.hughes@cancer.org.uk>
2013-11-01 16:47:08 +0000

    Added counter

--- app/count.js
+++ app/count.js
@@ -3,6 +3,23 @@ if (typeof define !== 'function') { var define = require('amdefine')(module); }
 define(function () {
   return {
     count : function (start, end) {
+      var timeout;
+      function next() {
+        console.log(start++);
+        if (start <= end) {
+          timeout = setTimeout(next, 100, start, end);
+        }
+      }
+
+      next();
+
+      return {
+        cancel: function() {
+          if (timeout) {
+            clearTimeout(timeout);
+          }
+        }
+      };
     }
   };
 });
